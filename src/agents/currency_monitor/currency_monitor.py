53c4fcbec491642ecd873cbbfda3da1ba1ffc69b
Mehak Singal <mehaksingal13@gmail.com>
2023-10-07 22:59:18 +0530

    created the currency exchange rate function

--- src/agents/currency_monitor/currency_monitor.py
+++ src/agents/currency_monitor/currency_monitor.py
@@ -1,9 +1,10 @@
 #import libraries
 import requests, json
 
-def currency_exchange_rate(from_currency, to_currency, api_key):    
-    url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=Z1WC0QSKNTR4JP1G'
+def currency_exchange_rate(from_currency, to_currency, api_key, country=True):  
+    url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency='+from_currency+'&to_currency='+to_currency+'&apikey='+api_key
     r = requests.get(url)
     data = r.json()
-    return data
+    return data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
 
+print(currency_exchange_rate("USD", "JPY", "YOUR_API_KEY"))
