d3007fb402db58f16dec0dbaaa6fa80c2b3126b3
Mohammed samir <abutech8801@gmail.com>
2023-10-10 23:03:00 +0530

    bug fixes

--- pages/Monitor_Currency_Exchange_Rate.py
+++ pages/Monitor_Currency_Exchange_Rate.py
@@ -16,23 +16,24 @@ header1 = st.container()
 header2 = st.container()
 user = st.container()
 
-#data
+# data
 user_data = pd.read_csv(os.path.join("src", "data", "user_settings.csv"))
-sleep(2)
+sleep(5)
 base_currency = user_data.Base_Currency.unique()[0]
 
-with open(os.path.join("src",".key","api_key.txt"), 'r') as f:
+with open(os.path.join("src", ".key", "api_key.txt"), 'r') as f:
     api_key = f.readline()
 
 alert_info = {}
 
 for idx, row in user_data.iterrows():
     try:
-        current_value = currency_exchange_rate(base_currency, row.Foreign_Currency, api_key)
+        current_value = currency_exchange_rate(
+            base_currency, row.Foreign_Currency, api_key)
 
         if row.Option == ">" and current_value > row.Threshold:
             alert_info[row.Foreign_Currency] = [">", row.Threshold]
-            
+
         if row.Option == "<" and current_value < row.Threshold:
             alert_info[row.Foreign_Currency] = ["<", row.Threshold]
     except:
@@ -49,12 +50,14 @@ with user:
     currency_options = user_data["Foreign_Currency"].tolist()
 
     graph_option = st.selectbox("Select Graph", options=graph_options, index=0)
-    currency = st.multiselect("Choose Currency", options=currency_options, default=currency_options)
+    currency = st.multiselect(
+        "Choose Currency", options=currency_options, default=currency_options)
 
     graph_data = user_data[user_data["Foreign_Currency"].isin(currency)]
 
     first_column, second_column = st.columns(2)
-    fig = px.bar(graph_data, x="Foreign_Currency", y=graph_option, color="Foreign_Currency")
+    fig = px.bar(graph_data, x="Foreign_Currency",
+                 y=graph_option, color="Foreign_Currency")
     fig.update_layout(width=450)
     first_column.write(fig)
 
@@ -64,11 +67,11 @@ with user:
             second_column.write("\n")
 
             if alert_info[foreign_currency][0] == ">":
-                second_column.info(f"Alert: {foreign_currency} exchange rate reached the value {user_data[user_data['Foreign_Currency'] == foreign_currency]['Threshold'].values[0]}")
-            
-            elif alert_info[foreign_currency][0] == "<":
-                second_column.info(f"Alert: {foreign_currency} exchange rate has fallen below the value {user_data[user_data['Foreign_Currency'] == foreign_currency]['Threshold'].values[0]}")
-            
-            play_sound()
-            
+                second_column.info(
+                    f"Alert: {foreign_currency} exchange rate reached the value {user_data[user_data['Foreign_Currency'] == foreign_currency]['Threshold'].values[0]}")
 
+            elif alert_info[foreign_currency][0] == "<":
+                second_column.info(
+                    f"Alert: {foreign_currency} exchange rate has fallen below the value {user_data[user_data['Foreign_Currency'] == foreign_currency]['Threshold'].values[0]}")
+
+            play_sound()
