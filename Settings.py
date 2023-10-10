e1cfd1610fac84c8ba74d6b37fc31fd417c2dc28
Mohammed samir <abutech8801@gmail.com>
2023-10-10 23:19:51 +0530

    added comments

--- Settings.py
+++ Settings.py
@@ -5,6 +5,7 @@ import pandas as pd
 import random, string
 from src.utils.functions import currency_exchange_rate
 
+# Page configuration
 st.set_page_config(
     page_title="Currency Exchange Monitor",
     layout="wide"
@@ -13,28 +14,19 @@ st.set_page_config(
 header = st.container()
 user_data = st.container()
 
-if 'count' not in st.session_state:
-	st.session_state.count = 0
-
 @st.cache_data
 def get_data(filename):
+    """
+    Reads data from a CSV file and returns it.
+    Parameters:
+        filename (str): The path to the CSV file to be read.
+    Returns:
+        pd.DataFrame: A Pandas DataFrame containing the data from the CSV file.
+    """
     data = pd.read_csv(filename)
     return data
 
 input_rows = []
-
-def add_row(foreign_currency, mode, threshold):
-    input_rows.append({"foreign_currency": foreign_currency, "mode": mode, "threshold": threshold})
-    foreign_currency = first_column.selectbox("Foreign Currency",
-                                              options=currency_codes,
-                                              index=1,
-                                              key=random.choice(string.ascii_uppercase)+str(random.randint(0,999999)))
-    mode = second_column.selectbox("",
-                                   options=[">", "<"],
-                                   index=0,
-                                   key=random.choice(string.ascii_uppercase)+str(random.randint(0,999999)))
-    threshold = third_column.text_input("Threshold",
-                                        key=random.choice(string.ascii_uppercase)+str(random.randint(0,999999)))
 
 data = get_data(os.path.join("src", "data", "currency_codes.csv"))
 currency_codes = data['Codes'].tolist()
@@ -42,6 +34,7 @@ currency_codes = data['Codes'].tolist()
 with header:
     st.title("Please Enter the Following Details")
 
+# Taking input from the user
 with user_data:
     api_key = st.text_input("API Key")
     base_currency = st.selectbox("Base Currency", options=currency_codes, index=0)
@@ -78,6 +71,7 @@ with user_data:
                 "Threshold": [],
                 "Exchange_Rate": []}
 
+        # Updating user_settings.csv
         for row in input_rows:
 
             d["Base_Currency"].append(base_currency)
--- pages/Monitor_Currency_Exchange_Rate.py
+++ pages/Monitor_Currency_Exchange_Rate.py
@@ -7,6 +7,7 @@ from src.utils.playSound import play_sound
 from src.utils.functions import currency_exchange_rate
 from time import sleep
 
+# Page configuration
 st.set_page_config(
     page_title="Currency Exchange Monitor",
     layout="wide"
@@ -16,7 +17,7 @@ header1 = st.container()
 header2 = st.container()
 user = st.container()
 
-# data
+# Data
 user_data = pd.read_csv(os.path.join("src", "data", "user_settings.csv"))
 sleep(5)
 base_currency = user_data.Base_Currency.unique()[0]
@@ -24,6 +25,7 @@ base_currency = user_data.Base_Currency.unique()[0]
 with open(os.path.join("src", ".key", "api_key.txt"), 'r') as f:
     api_key = f.readline()
 
+# Populating alert info
 alert_info = {}
 
 for idx, row in user_data.iterrows():
@@ -45,7 +47,10 @@ with header1:
 with header2:
     st.text(f"Base Currency: {base_currency}")
 
+# Monitor
 with user:
+
+    # Creating graphs
     graph_options = ["Exchange_Rate", "Threshold"]
     currency_options = user_data["Foreign_Currency"].tolist()
 
@@ -61,6 +66,7 @@ with user:
     fig.update_layout(width=450)
     first_column.write(fig)
 
+    # Generating alerts based on conditions
     if alert_info:
         second_column.markdown("<br>" * 3, unsafe_allow_html=True)
         for foreign_currency in alert_info.keys():
